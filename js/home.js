08543c5fb9b7c85ab208fff445e67e762ecaaed3
Galaxy-you <violeteternal@whut.edu.cn>
2026-04-28 21:48:26 +0800

    完成主页 + 世界观

--- js/home.js
+++ js/home.js
@@ -6,10 +6,29 @@
   if (!raw) return;
   var list = raw.split("|").filter(Boolean);
   if (list.length < 2) return;
+  var dots = document.querySelectorAll("[data-slide-dot]");
 
   var index = 0;
+  function render(nextIndex) {
+    index = nextIndex;
+    heroImage.src = list[index];
+    dots.forEach(function (dot) {
+      var dotIndex = Number(dot.getAttribute("data-slide-dot"));
+      dot.classList.toggle("is-active", dotIndex === index);
+    });
+  }
+
+  dots.forEach(function (dot) {
+    dot.addEventListener("click", function () {
+      var dotIndex = Number(dot.getAttribute("data-slide-dot"));
+      if (dotIndex >= 0 && dotIndex < list.length) {
+        render(dotIndex);
+      }
+    });
+  });
+
   window.setInterval(function () {
     index = (index + 1) % list.length;
-    heroImage.src = list[index];
+    render(index);
   }, 5500);
 })();
